4e99593008b3000429f2bc1065ef678ffcde20bd
harsh <harshgowdaujire@gmail.com>
2026-06-11 16:49:36 +0530

    df

--- magnific-frontend/src/pages/Home.jsx
+++ magnific-frontend/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './Home.css';
 import ledImage from '../assets/led.jpg';
 import Hero from './Hero';
-
+import cha from '../assets/cha.jpg';
+import led from '../assets/LED Wall Light (23).jpg';
 export default function Home() {
   return (
     
@@ -10,20 +11,39 @@ export default function Home() {
      
       <Hero />
       
-      <section className="features-section flex flex-row">
-
-
-
-        <div className="container-features  px-4 flex flex-col  justify-center">
-          <h3 className="text-1xl font-light  mb-4">Key Features</h3>
-          <h2 className="text-3xl font-semibold mb-8">Why Choose Magnific Fans?</h2>
-          <p className="text-lg  mb-8">Our fans are designed to provide superior airflow while adding a touch of elegance to your space. With advanced technology and sleek designs, Magnific Fans are the perfect choice for any room.</p>
-          <button className="bg-blue-500 text-white px-1 py-3 rounded-2xl w-40">Learn More</button>
-
+      <section className="features-section flex flex-col">
+        <div className="product-container flex flex-row items-center gap-20  mb-20">
+          <div className="product-content">
+            <h1>Introducing the Magnific Fan</h1>
+          </div>  
+          <div className="product-description">
+            <p className='text-2xl'>Our fans are designed to provide superior airflow while <br></br>adding a touch of elegance to your space.</p>
+          </div>
         </div>
-        <div className="container-features">
-          <img src={ledImage} alt="Feature Image" className="w-full h-auto rounded-4xl shadow-lg" />
+
+       < div className="features-image-container flex flex-row gap-4 ">
+          <div className="features-image-wrapper ">
+            <img src={cha} alt="Magnific Fan" className="features-image" />
+            <h1>Feature 1</h1>
+          </div>
+          <div className="features-image-wrapper">
+            <img src={cha} alt="Magnific Fan" className="features-image" />
+            <h1>Feature 2</h1>  
+          
+          </div>
+          <div className="features-image-wrapper">
+            <img src={cha} alt="Magnific Fan" className="features-image" />
+            <h1>Feature 3</h1>
+          </div>
+          <div className="features-image-wrapper">
+            <img src={cha} alt="Magnific Fan" className="features-image" />
+            <h1>Feature 4</h1>
+          </div>
+         
         </div>
+
+
+        
      
       </section>
          <div className="container-features  px-4 flex flex-col  justify-center">
